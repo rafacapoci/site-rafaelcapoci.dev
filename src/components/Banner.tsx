54a01b5bfa5cffcdb6696e9b2b54091edb35519a
Rafael Capoci <rafaelcapoci@gmail.com>
2024-03-27 01:33:05 -0300

    Adicoes de informacoes

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,7 +7,7 @@ export default function Banner() {
             <p className="text-white max-w-56 ml-8 text-lg pb-24">
                 Site desenvolvido inteiramente por mim. Pensado em mobile first então para uma melhor experiencia, visualize por um Smartphone.
             </p>
-            <div className="inline-flex text-white/50 font-bold text-1xl flex-wrap justify-center">
+            <div className="inline-flex text-white/50 font-bold text-1xl flex-wrap justify-center m-8">
                 <p className="px-3">AWS</p>
                 <p className="px-3">EC2</p>
                 <p className="px-3">DevOps</p>
